2f6c092144bb647284abd9ebbff1aa2da0acc1e3
Bastien <bzg@gnu.org>
2020-03-11 19:13:48 +0100

    Don't update when no update is available upstream

--- src/core.clj
+++ src/core.clj
@@ -11,30 +11,21 @@
             [java-time :as t])
   (:gen-class))
 
-(defonce http-get-params {:cookie-policy :standard})
-
 (def data-url "https://www.santepubliquefrance.fr/maladies-et-traumatismes/maladies-et-infections-respiratoires/infection-a-coronavirus/articles/infection-au-nouveau-coronavirus-sars-cov-2-covid-19-france-et-monde")
 
 (defn get-covid19-raw-data []
-  (if-let [data (try (http/get data-url http-get-params)
+  (if-let [data (try (http/get data-url {:cookie-policy :standard})
                      (catch Exception _ nil))]
-    (let [out (-> data
-                  :body
-                  h/parse
-                  h/as-hickory
+    (let [out (-> data :body h/parse h/as-hickory
                   (as-> d (hs/select (hs/class "content__table-inner") d))
-                  first
-                  :content
-                  first
-                  :content)]
+                  first :content first :content)]
       (:content (second out)))))
 
 (defn get-covid19-data []
-  (filter
-   (fn [[_ c]] (string? c))
-   (map (fn [[l r]]
-          [(first (:content l)) (first (:content r))])
-        (map :content (get-covid19-raw-data)))))
+  (filter (fn [[_ c]] (string? c))
+          (map (fn [[l r]]
+                 [(first (:content l)) (first (:content r))])
+               (map :content (get-covid19-raw-data)))))
 
 (defn arrange-data [data]
   [(cons "Date" (map first data))
@@ -42,18 +33,16 @@
          (map second data))])
 
 (defn -main []
-  (let [hist       (try (with-open [reader (io/reader "covid19.csv")]
-                          (doall
-                           (csv/read-csv reader)))
-                        (catch Exception _
-                          (println "Creating covid19.csv file")))
-        header     (not-empty (take 1 hist))
-        hist-rows  (rest hist)
-        new        (arrange-data (get-covid19-data))
-        new-header (take 1 new)
-        new-rows   (rest new)]
-    (with-open [writer (io/writer "covid19.csv")]
-      (csv/write-csv
-       writer (concat (or header new-header)
-                      (distinct (concat hist-rows new-rows)))))
-    (println "Wrote covid19.csv")))
+  (let [hist (try (with-open [reader (io/reader "covid19.csv")]
+                    (doall
+                     (csv/read-csv reader)))
+                  (catch Exception _
+                    (println "Creating covid19.csv file")))
+        new  (arrange-data (get-covid19-data))]
+    (if (= (drop 1 (last hist)) (drop 1 (last new)))
+      (println "No update available")
+      (do (with-open [writer (io/writer "covid19.csv")]
+            (csv/write-csv
+             writer (concat (or (not-empty (take 1 hist)) (take 1 new))
+                            (distinct (concat (rest hist) (rest new))))))
+          (println "Wrote covid19.csv")))))
